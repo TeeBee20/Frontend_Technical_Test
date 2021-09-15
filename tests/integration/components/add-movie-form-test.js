76f54b78d21141f011fb8bba848bfd096c6003c6
TeeBee20 <TiffanyBrooks2013@gmail.com>
2021-09-15 15:21:22 -0500

    add render test for title label

--- tests/integration/components/add-movie-form-test.js
+++ tests/integration/components/add-movie-form-test.js
@@ -2,3 +2,11 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Component | add-movie-form', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('title label has correct text', async function (assert) {
+    await render(hbs`<AddMovieForm />`);
+  });
+});
