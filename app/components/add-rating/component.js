c74d8d9d0e6cd45b9d42060009028ced4896cd3b
TeeBee20 <TiffanyBrooks2013@gmail.com>
2021-09-16 18:58:13 -0500

    define styleNamespace property

--- app/components/add-rating/component.js
+++ app/components/add-rating/component.js
@@ -1,3 +1,6 @@
 import Component from '@glimmer/component';
+import podNames from 'ember-component-css/pod-names';
 
-export default class AddRatingComponent extends Component {}
+export default class AddRatingComponent extends Component {
+  styleNamespace = podNames['add-rating'];
+}
