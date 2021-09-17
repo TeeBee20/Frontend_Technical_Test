d27cb7a73aba8d1544be492bb24e50cd6db0937f
TeeBee20 <TiffanyBrooks2013@gmail.com>
2021-09-17 12:34:19 -0500

    define isEditing prop

--- app/components/movie-list/movie-list-item/component.js
+++ app/components/movie-list/movie-list-item/component.js
@@ -6,6 +6,8 @@ import podNames from 'ember-component-css/pod-names';
 export default class MovieListItem extends Component {
   styleNamespace = podNames['movie-list/movie-list-item'];
 
+  @tracked isEditing = false;
+
   get movie() {
     // adds ref property on movie in order to access doc ref property for edit & delete feature
     const movieDataRef = this.args.movie.data();
