15401115ad28f714aba4cc69c21ca701cfa16b68
TeeBee20 <TiffanyBrooks2013@gmail.com>
2021-09-17 10:49:38 -0500

    add logic to movie getter that adds ref property for document

--- app/components/movie-list/movie-list-item/component.js
+++ app/components/movie-list/movie-list-item/component.js
@@ -5,6 +5,9 @@ export default class MovieListItem extends Component {
   styleNamespace = podNames['movie-list/movie-list-item'];
 
   get movie() {
-    return this.args.movie.data();
+    // adds ref property on movie in order to access doc ref property for edit & delete feature
+    const movieDataRef = this.args.movie.data();
+    movieDataRef.ref = this.args.movie.ref;
+    return movieDataRef;
   }
 }
