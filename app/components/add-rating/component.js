a536ad4ae0d748e729bc11cdfea93141d43235a5
TeeBee20 <TiffanyBrooks2013@gmail.com>
2021-09-16 20:43:00 -0500

    remove add-rating component directory and test file

--- app/components/add-rating/component.js
+++
@@ -1,6 +0,0 @@
-import Component from '@glimmer/component';
-import podNames from 'ember-component-css/pod-names';
-
-export default class AddRating extends Component {
-  styleNamespace = podNames['add-rating'];
-}
