455fca2753aebb633a20405ef8223aea46706eaa
TeeBee20 <TiffanyBrooks2013@gmail.com>
2021-09-16 19:04:49 -0500

    rename add-rating component

--- app/components/add-rating/component.js
+++ app/components/add-rating/component.js
@@ -1,6 +1,6 @@
 import Component from '@glimmer/component';
 import podNames from 'ember-component-css/pod-names';
 
-export default class AddRatingComponent extends Component {
+export default class AddRating extends Component {
   styleNamespace = podNames['add-rating'];
 }
