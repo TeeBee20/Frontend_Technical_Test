1b6b7f9f61f46cc44a02cc2f6d2c894ecf3a2f1d
TeeBee20 <TiffanyBrooks2013@gmail.com>
2021-09-19 12:46:30 -0500

    add conditional logic to loadMovies thatperforms orderBy query based on sort order

--- app/components/landing-page/component.js
+++ app/components/landing-page/component.js
@@ -1,5 +1,11 @@
 import { action } from '@ember/object';
-import { collection, getFirestore, getDocs } from 'firebase/firestore';
+import {
+  collection,
+  getFirestore,
+  getDocs,
+  query,
+  orderBy,
+} from 'firebase/firestore';
 import { tracked } from '@glimmer/tracking';
 import Component from '@glimmer/component';
 import podNames from 'ember-component-css/pod-names';
@@ -26,6 +32,7 @@ export default class LandingPage extends Component {
 
   @action sortMoviesByRating(event) {
     this.sortOrder = event.target.value;
+    this.loadMovies();
   }
 
   @action async loadMovies() {
@@ -34,7 +41,26 @@ export default class LandingPage extends Component {
     const moviesSnapshot = await getDocs(moviesRef);
     const movies = [];
 
-    moviesSnapshot.forEach((doc) => movies.push(doc));
+    if (this.sortOrder === 'asc') {
+      //query that orders by rating asc
+      const q = query(moviesRef, orderBy('rating'));
+      const querySnapshot = await getDocs(q);
+
+      querySnapshot.forEach((doc) => {
+        movies.push(doc);
+      });
+    } else if (this.sortOrder === 'desc') {
+      //query that orders by rating desc
+      const q = query(moviesRef, orderBy('rating', 'desc'));
+      const querySnapshot = await getDocs(q);
+
+      querySnapshot.forEach((doc) => {
+        movies.push(doc);
+      });
+    } else {
+      //default behavior that displays movies when no order chosen
+      moviesSnapshot.forEach((doc) => movies.push(doc));
+    }
 
     this.movies = movies;
   }
